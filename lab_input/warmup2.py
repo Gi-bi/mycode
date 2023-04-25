8b52aac47fd3d939cba6a2c2b1e291b1e8bea00e
Gi-bi <stanjessgrubbs@gmail.com>
2023-04-25 20:01:57 +0000

    join and split

---
+++ lab_input/warmup2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def calculator():
+   num1= input("Enter your first number: " )
+   num2= input("Enter your second number: " )
+   print("Total = ", int(num1) + int(num2))
+
+calculator()
