b7c787e319389364f6d0ccc3eb1f1240a9656980
Gi-bi <stanjessgrubbs@gmail.com>
2023-05-03 19:56:16 +0000

    my first flask app

---
+++ pokemon/lab91.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import requests
+
+def main():
+    pokenum= input("Pick a number between 1 and 151!\n>")
+    pokeapi= requests.get("https://pokeapi.co/api/v2/pokemon/" + pokenum).json()
+
+
+    for x in pokeapi["moves"]:
+        print(x["move"]["name"])
+
+    print(pokeapi["sprites"]["front_default"])
+
+    
+
+main()
+
